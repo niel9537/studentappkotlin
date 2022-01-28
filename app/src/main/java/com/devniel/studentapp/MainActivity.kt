fe2e12697761a5b3e5f31747e558e8b8da7a14eb
LAPTOP-DHA0R3LE\Niel <niel.zebua8@gmail.com>
2022-01-28 20:37:42 +0700

    fetch ke recyclerview berhasil

--- app/src/main/java/com/devniel/studentapp/MainActivity.kt
+++ app/src/main/java/com/devniel/studentapp/MainActivity.kt
@@ -3,17 +3,34 @@ package com.devniel.studentapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private val api by lazy { ApiRetrofit().endpoint }
+    private lateinit var valuesAdapter: ValuesAdapter
+    private lateinit var listValue: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupList()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        getValue()
+    }
+
+    private fun setupList(){
+        listValue = findViewById(R.id.list_item)
+        valuesAdapter = ValuesAdapter(arrayListOf())
+        listValue.adapter = valuesAdapter
+    }
+
+    private fun getValue(){
         api.data().enqueue(object : Callback<ValuesModel>{
             override fun onFailure(call: Call<ValuesModel>, t: Throwable) {
                 Log.e("MainActivity",t.toString())
@@ -22,9 +39,10 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<ValuesModel>, response: Response<ValuesModel>) {
                 if(response.isSuccessful){
                     val listData = response.body()!!.values
-                    listData.forEach({
-                        Log.e("MainActivity","Nama : ${it.nama}")
-                    })
+                    valuesAdapter.setData(listData)
+//                    listData.forEach({
+//                        Log.e("MainActivity","Nama : ${it.nama}")
+//                    })
                 }
 
             }
---
+++ app/src/main/java/com/devniel/studentapp/ValuesAdapter.kt
@@ -0,0 +1,34 @@
+package com.devniel.studentapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+class ValuesAdapter(
+    val students: ArrayList<ValuesModel.Data>
+): RecyclerView.Adapter<ValuesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_mahasiswa, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ValuesAdapter.ViewHolder, position: Int) {
+        val data = students[position]
+        holder.textStudent.text = data.nama
+    }
+
+    override fun getItemCount() = students.size
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val textStudent = view.findViewById<TextView>(R.id.txtMahasiswa)
+        val btnDel = view.findViewById<TextView>(R.id.btnDelete)
+    }
+
+    public fun setData(data: List<ValuesModel.Data>){
+        students.clear()
+        students.addAll(data)
+        notifyDataSetChanged()
+    }
+}
